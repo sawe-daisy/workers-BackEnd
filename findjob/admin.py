ab5df6adfbfaaa9dd65e176b1e97ef187c8aeebd
Mike Munene <you@example.com>
2020-12-17 16:26:17 +0300

    models registered

--- findjob/admin.py
+++ findjob/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
+from .models import User, Jobpost, Reviews, Profile, jobcategory
+from django.contrib import admin
 
 # Register your models here.
+admin.site.register(User)
+admin.site.register(Jobpost)
+admin.site.register(jobcategory)
+admin.site.register(Profile)
+admin.site.register(Reviews)
+
+
+class Review_Admin(admin.ModelAdmin):
+    list_display = ('name', 'body', 'post', 'created_on', 'active')
+    list_filter = ('active', 'created_on')
+    search_fields = ('name', 'email', 'body')
+    actions = ['approve_reviews']
+
+    def approve_reviews(self, request, queryset):
+        queryset.update(active=True)
